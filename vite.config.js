635e8f97727e8553d4c6fdd1db91520f54660b48
Riccardo Macchi <rick.mac97@outlook.com>
2025-01-27 17:48:08 +0100

    feat(deploy)

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,38 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: '0.0.0.0',  // Esponi il server su tutte le interfacce di rete
-    port: 5173,        // Porta predefinita, puoi cambiarla se necessario
+  plugins: [
+    vue(),
+    VitePWA({
+      base: '/fit-app/', // Sostituisci con il nome della tua repository
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'fit-app',
+        short_name: 'App',
+        description: 'Descrizione della tua Progressive Web App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/images/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/images/icons/icon-592x592.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    outDir: 'dist',
   },
-})
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+});
