3be1ec42f36596cd11418ad12cb4d75fbae0c7ae
Riccardo Macchi <rick.mac97@outlook.com>
2025-01-27 16:32:58 +0100

    feat(deploy): add PWA and deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Nome della tua app',
+        short_name: 'App',
+        description: 'Descrizione della tua Progressive Web App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/images/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/images/icons/icon-592x592.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  base: '/fit-app/',
   server: {
-    host: '0.0.0.0',  // Esponi il server su tutte le interfacce di rete
-    port: 5173,        // Porta predefinita, puoi cambiarla se necessario
+    host: '0.0.0.0',
+    port: 5173,
   },
-})
+});
